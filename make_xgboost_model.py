ca182055a6b0adf1d1f6f5c148a5b7d899c47021
NoeSturm <37624237+NoeSturm@users.noreply.github.com>
2019-12-04 22:58:01 +0100

    Update make_xgboost_model.py

--- make_xgboost_model.py
+++ make_xgboost_model.py
@@ -10,8 +10,6 @@ import os,sys
 import pandas as pd
 import pickle
 
-os.chdir("/projects/cc/kxsw653/ExCAPE-ML_v5/ML_models_results/results_v5_xgboost_ecfp")
-
 if len(sys.argv)!=7: 
     print("Usage: {} [target_idx] [obj] [lr] [scale_pos_w] [n_Estim] [max_depth]".format(sys.argv[0]) )
     quit()
@@ -62,7 +60,7 @@ model.fit(X_train, y_train)
 #predictions['hyperparam'] = hyperparam_str
 #predictions['target'] = target_idx
 
-WORKDIR="/projects/cc/kxsw653/ExCAPE-ML_v5/ML_models_results/results_v5_xgboost_ecfp"
+WORKDIR="results_v5_xgboost_ecfp"
 
 #print("predictions saved in {}/nested-cv-predictions/predictions-1-1-1-6-{}.pickle".format(WORKDIR, target_idx))
 #predictions.to_pickle("{}/nested-cv-predictions/predictions-1-1-1-6-{}.pickle".format(WORKDIR,target_idx))
